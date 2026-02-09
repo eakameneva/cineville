5096d700c51f4b8a3329c1ede71b0e3f52ec4cf1
eakameneva <kate103422@gmail.com>
2026-02-09 18:50:07 +0100

    add: event list item info

--- src/components/EventListItem.tsx
+++ src/components/EventListItem.tsx
@@ -1,13 +1,31 @@
 import type { FC } from "react";
 import type { Event } from "../types";
+import { format } from "date-fns";
+import placeholderPoster from "../assets/placeholderPoster.png";
+import { Link } from "react-router";
 
 interface IEventListItemProps {
   event: Event;
 }
 export const EventListItem: FC<IEventListItemProps> = ({ event }) => {
   return (
-    <li className="border-2 p-5">
-      <p>{event._embedded.production?.title}</p> <p>{event.startDate}</p>
+    <li className="bg-white p-4 shadow hover:shadow-lg transition-shadow cursor-pointer flex gap-5">
+      <Link to={`details/${event.id}`}>
+        <img
+          className="max-w-2xs aspect-video object-cover"
+          src={
+            event._embedded.production?.assets?.cover?.url
+              ? event._embedded.production.assets.cover.url
+              : placeholderPoster
+          }
+          alt="Cover"
+        />
+        <div>
+          <h3>{event._embedded.production?.title}</h3>
+          <p>{format(event.startDate, "HH:mm")}</p>
+          <p>{event._embedded.venue?.name}</p>
+        </div>
+      </Link>
     </li>
   );
 };
