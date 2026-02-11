19b062c8449e00aa9b616e063ec9ae2387e4a99d
eakameneva <kate103422@gmail.com>
2026-02-11 17:29:56 +0100

    fix: changed event filters logic, added styling

--- src/components/EventFilters.tsx
+++ src/components/EventFilters.tsx
@@ -1,57 +1,72 @@
-import type { FC } from "react";
 import { format, isToday } from "date-fns";
+
+import type { ChangeEvent, FC } from "react";
 
 const DAY_OPTION_AMOUNT = 7;
 const FULL_DAY_HOURS = 24;
 const EVENT_START_HOUR = 9;
 
+const selectStyles =
+  "rounded-md px-3 py-2 cursor-pointer border border-gray-300";
+
+const getDayOptionTitle = (date: Date, index: number) => {
+  if (index === 0) {
+    return "TODAY";
+  }
+
+  return format(date, "d MMM");
+};
+
 const getDayFilterOptions = () => {
-  const dayOptions = new Array(DAY_OPTION_AMOUNT).fill(null);
+  const dayOptions = Array.from({ length: DAY_OPTION_AMOUNT });
 
   return dayOptions.map((_, index) => {
     const date = new Date();
-    let title = "";
 
     date.setDate(date.getDate() + index);
 
-    if (index === 0) {
-      title = "TODAY";
-    } else {
-      title = format(date, "d MMM");
-    }
-
-    return { value: format(date, "yyyy-MM-dd"), title };
+    return {
+      value: format(date, "yyyy-MM-dd"),
+      title: getDayOptionTitle(date, index),
+    };
   });
 };
 
 const getStartHour = (dateFilterValue: Date) => {
   const date = new Date();
+
   if (isToday(dateFilterValue)) {
     return date.getHours();
   }
+
   return EVENT_START_HOUR;
 };
 
-const getTimeFilterOptions = (dateFilterValue: Date) => {
-  const date = new Date();
-  let startHour = EVENT_START_HOUR;
-  const isTodayFilterSelected = isToday(dateFilterValue);
-
-  if (isTodayFilterSelected) {
-    startHour = date.getHours();
+const getTimeOptionTitle = (
+  index: number,
+  dateFilterValue: Date,
+  hours: number
+) => {
+  if (index === 0 && isToday(dateFilterValue)) {
+    return "NOW";
+  } else if (index === 0) {
+    return "ALL DAY";
   }
-  const hourOptions = new Array(FULL_DAY_HOURS - startHour).fill(null);
+
+  return `${hours}:00`;
+};
+
+const getTimeFilterOptions = (dateFilterValue: Date) => {
+  const startHour = getStartHour(dateFilterValue);
+  const hourOptions = Array.from({ length: FULL_DAY_HOURS - startHour });
 
   return hourOptions.map((_, index) => {
-    let title = "";
-    const optionHours = startHour + index;
+    const value = startHour + index;
 
-    if (index === 0) {
-      title = isTodayFilterSelected ? "NOW" : "ALL DAY";
-    } else {
-      title = `${optionHours}:00`;
-    }
-    return { value: optionHours, title };
+    return {
+      value,
+      title: getTimeOptionTitle(index, dateFilterValue, value),
+    };
   });
 };
 
@@ -64,42 +79,63 @@ export const EventFilters: FC<IEventFiltersProps> = ({
   dateFilterValue,
   onDateFilterValueChange,
 }) => {
+  const getIsCurrentHourSelected = (selectedDate: Date) => {
+    const currentDate = new Date();
+    const selectedHours = selectedDate.getHours();
+
+    return isToday(selectedDate) && currentDate.getHours() === selectedHours;
+  };
+
+  const handleFilterChange = (date: Date, hours: number) => {
+    const newDate = new Date(date);
+
+    newDate.setHours(hours);
+
+    if (getIsCurrentHourSelected(newDate)) {
+      onDateFilterValueChange(new Date());
+      return;
+    }
+
+    onDateFilterValueChange(newDate);
+  };
+
+  const handleDaysFilterChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    const newDate = new Date(event.target.value);
+
+    handleFilterChange(newDate, getStartHour(newDate));
+  };
+
+  const handleTimeFilterChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    const dateWithNewTime = new Date(dateFilterValue);
+
+    handleFilterChange(dateWithNewTime, +event.target.value);
+  };
+
   return (
-    <div>
+    <div className="flex gap-4 sticky top-0 bg-[#fffbf4] py-4 px-6 z-10">
       <select
         name="days"
-        onChange={(event) => {
-          const newDate = new Date(event.target.value);
-          newDate.setHours(getStartHour(newDate));
-          onDateFilterValueChange(newDate);
-        }}
+        value={format(dateFilterValue, "yyyy-MM-dd")}
+        onChange={handleDaysFilterChange}
+        className={selectStyles}
       >
         {getDayFilterOptions().map((day) => {
           return (
-            <option
-              value={day.value}
-              selected={day.value === format(dateFilterValue, "yyyy-MM-dd")}
-            >
+            <option key={day.value} value={day.value}>
               {day.title}
             </option>
           );
         })}
       </select>
-
       <select
         name="time"
-        onChange={(event) => {
-          const dateWithNewTime = new Date(dateFilterValue);
-          dateWithNewTime.setHours(+event.target.value);
-          onDateFilterValueChange(dateWithNewTime);
-        }}
+        value={dateFilterValue.getHours()}
+        onChange={handleTimeFilterChange}
+        className={selectStyles}
       >
         {getTimeFilterOptions(dateFilterValue).map((time) => {
           return (
-            <option
-              value={time.value}
-              selected={time.value === dateFilterValue.getHours()}
-            >
+            <option key={time.value} value={time.value}>
               {time.title}
             </option>
           );
