b23fe74fa3a321b0ae0ed9c9ef8d0db59cdb8bcc
eakameneva <kate103422@gmail.com>
2026-02-11 18:42:01 +0100

    add: event details component with styles

---
+++ src/components/EventDetails.tsx
@@ -0,0 +1,103 @@
+import type { FC } from "react";
+import type { Event } from "../types";
+import { format } from "date-fns";
+import placeholderPoster from "../assets/placeholderPoster.png";
+import { formatLanguages } from "../utils";
+import { Link } from "react-router";
+
+interface IEventDetailsProps {
+  event: Event;
+}
+export const EventDetails: FC<IEventDetailsProps> = ({ event }) => {
+  const { production, venue } = event._embedded;
+  const { startDate, attributes } = event;
+
+  if (!production) return null;
+
+  const { title, assets, localizableAttributes } = production;
+
+  return (
+    <div className="max-w-5xl mx-auto p-6">
+      <Link to="/" className="inline-block mb-6 font-medium">
+        ← Back to schedule
+      </Link>
+
+      <h1 className="text-3xl font-bold text-gray-900 mb-6">Show details</h1>
+      <div className="bg-white rounded-lg shadow-lg p-6">
+        <img
+          className="w-full max-w-2xl aspect-video object-cover rounded-lg mb-6"
+          src={assets?.cover?.url || placeholderPoster}
+          alt={`${title} poster`}
+        />
+
+        <h2 className="text-2xl font-bold text-gray-900 mb-4">{title}</h2>
+        <div className="space-y-3 text-gray-700">
+          <p>
+            {format(startDate, "d MMMM")} {format(startDate, "HH:mm")}
+          </p>
+
+          {production.attributes.duration && (
+            <p>
+              <span className="font-semibold">Duration:</span>{" "}
+              {production.attributes.duration} minutes
+            </p>
+          )}
+
+          {!!production.attributes.spokenLanguages?.length && (
+            <p>
+              <span className="font-semibold">Language:</span>{" "}
+              {formatLanguages(production?.attributes.spokenLanguages)}
+            </p>
+          )}
+          {!!attributes.subtitles?.length && (
+            <p>
+              <span className="font-semibold">Subtitles:</span>{" "}
+              {formatLanguages(attributes.subtitles)}
+            </p>
+          )}
+
+          {!!production.attributes.directors?.length && (
+            <p>
+              <span className="font-semibold">Directed by:</span>{" "}
+              {production.attributes.directors.join(", ")}
+            </p>
+          )}
+
+          {!!production.attributes.cast?.length && (
+            <p>
+              <span className="font-semibold">Starring:</span>{" "}
+              {production.attributes.cast.join(", ")}
+            </p>
+          )}
+
+          {production.attributes.releaseYear && (
+            <p>
+              <span className="font-semibold">Release year:</span>{" "}
+              {production.attributes.releaseYear}
+            </p>
+          )}
+
+          {venue && (
+            <div className="pt-4 border-t border-gray-200">
+              <p className="font-semibold text-lg mb-2">Cinema</p>
+              <p>{venue.name}</p>
+              <p className="text-gray-600">
+                {venue.address.street} {venue.address.houseNumber},{" "}
+                {venue.address.city}
+              </p>
+            </div>
+          )}
+
+          {!!localizableAttributes?.description && (
+            <div className="pt-4 border-t border-gray-200">
+              <p className="font-semibold text-lg mb-2">Description</p>
+              <p className="text-gray-600">
+                {localizableAttributes.description}
+              </p>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/pages/EventDetailsPage.tsx
+++ src/pages/EventDetailsPage.tsx
@@ -1,93 +1,34 @@
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router";
 import { getEvent } from "../api/requests";
-import placeholderPoster from "../assets/placeholderPoster.png";
-import { format } from "date-fns";
+import { EventDetails } from "../components/EventDetails";
 
 export const EventDetailsPage = () => {
   const { eventId = "" } = useParams();
 
-  const { data: event, isLoading } = useQuery({
+  const {
+    data: event,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
     queryKey: ["events", eventId],
     queryFn: () => getEvent(eventId),
   });
 
-  if (!event && isLoading) {
-    return <div>Loading</div>;
+  if (isLoading) {
+    return <div className="text-center py-12">Loading...</div>;
+  }
+  if (isError) {
+    return <div className="text-center py-12">Error: {error.message}</div>;
   }
   if (!event) {
-    return <p>Error loading event</p>;
+    return <div className="text-center py-12">Event Not Found</div>;
   }
-  const { production, venue } = event._embedded;
-  const { startDate, attributes } = event;
 
-  const formatLanguages = (languages: string[] = []) => {
-    const displayNames = new Intl.DisplayNames(["en"], {
-      type: "language",
-    });
-
-    return languages
-      .map((lang) => {
-        const baseLang = lang.split("-")[0];
-        return displayNames.of(baseLang) ?? lang;
-      })
-      .join(", ");
-  };
   return (
-    <div className="max-w-5xl mx-auto p-6">
-      <h2>Event details</h2>
-      <img
-        className="max-w-2xs aspect-video object-cover"
-        src={
-          production?.assets?.cover?.url
-            ? production.assets.cover.url
-            : placeholderPoster
-        }
-        alt="Film poster"
-      />
-      <p>{format(startDate, "d MMMM")}</p>
-      <p>{format(startDate, "HH:mm")}</p>
-      <p>{production?.title}</p>
-
-      {!!production?.attributes.releaseYear && (
-        <p>
-          <span>Release year:</span> {production.attributes.releaseYear}
-        </p>
-      )}
-
-      {!!production?.attributes.spokenLanguages?.length && (
-        <p>
-          <span>Language:</span>{" "}
-          {formatLanguages(production?.attributes.spokenLanguages)}
-        </p>
-      )}
-      {!!attributes.subtitles?.length && (
-        <p>
-          <span>Subtitles:</span> {formatLanguages(attributes.subtitles)}
-        </p>
-      )}
-
-      {!!production?.attributes.directors?.length && (
-        <p>
-          <span>Directed by:</span> {production.attributes.directors.join(", ")}
-        </p>
-      )}
-
-      {!!production?.attributes.cast?.length && (
-        <p>
-          <span>Starring:</span> {production.attributes.cast.join(", ")}
-        </p>
-      )}
-
-      <p>{event?._embedded.venue?.name}</p>
-      <p>
-        {venue?.address.street} {venue?.address.houseNumber},{" "}
-        {venue?.address.city}
-      </p>
-
-      {!!production?.localizableAttributes?.description && (
-        <p>{production?.localizableAttributes.description}</p>
-      )}
+    <div className="min-h-screen bg-[#fffbf4]">
+      <EventDetails event={event} />
     </div>
   );
 };
